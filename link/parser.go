bba911d08021b51342b05f9a04864f0b78669fd8
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2023-11-12 15:33:28 -0400

    Reimplement recursive DFS due to simplicity

--- link/parser.go
+++ link/parser.go
@@ -9,8 +9,7 @@ import (
 
 // Parser struct to handle parsing of links
 type Parser struct {
-	doc   *html.Node
-	links []Link
+	doc *html.Node
 }
 
 // Link struct containing information about a parsed link
@@ -27,64 +26,45 @@ func NewParser(reader io.Reader) (*Parser, error) {
 	}
 
 	return &Parser{
-		doc:   doc,
-		links: make([]Link, 0),
+		doc: doc,
 	}, nil
 }
 
 // ExtractLinks returns a list of parsed from a HTML document
 func (p *Parser) ExtractLinks() []Link {
-	links := make([]Link, 0)
-	stack := make([]*html.Node, 0)
+	anchorTags := searchAnchorTags(p.doc)
 
-	stack = append([]*html.Node{p.doc}, stack...)
+	links := make([]Link, 0, len(anchorTags))
 
-	for len(stack) != 0 {
-		currentNode := stack[0]
+	for _, anchorTag := range anchorTags {
+		link := newLink(anchorTag)
 
-		if len(stack) > 1 {
-			stack = stack[1:]
-		} else {
-			stack = stack[:0]
-		}
-
-		if currentNode.Type == html.ElementNode && currentNode.Data == "a" {
-			link := newLink(currentNode)
-
-			links = append(links, link)
-		}
-
-		if currentNode.NextSibling != nil {
-			stack = append([]*html.Node{currentNode.NextSibling}, stack...)
-		}
-
-		if currentNode.FirstChild != nil {
-			stack = append([]*html.Node{currentNode.FirstChild}, stack...)
-		}
+		links = append(links, link)
 	}
 
 	return links
 }
 
-func (p *Parser) search2(node *html.Node) {
-	if node.Type == html.ElementNode && node.Data == "a" {
-		link := newLink(node)
+func searchAnchorTags(node *html.Node) []*html.Node {
+	anchorTags := make([]*html.Node, 0)
 
-		p.links = append(p.links, link)
+	if node.Type == html.ElementNode && node.Data == "a" {
+		anchorTags = append(anchorTags, node)
 	}
 
 	if node.FirstChild != nil {
-		p.search2(node.FirstChild)
+		anchorTags = append(anchorTags, searchAnchorTags(node.FirstChild)...)
 	}
 
 	if node.NextSibling != nil {
-		p.search2(node.NextSibling)
+		anchorTags = append(anchorTags, searchAnchorTags(node.NextSibling)...)
 	}
+
+	return anchorTags
 }
 
 func newLink(node *html.Node) Link {
 	href := getHref(node.Attr)
-
 	textParts := extractTextParts(node)
 
 	return Link{
@@ -95,33 +75,20 @@ func newLink(node *html.Node) Link {
 
 func extractTextParts(node *html.Node) []string {
 	textParts := make([]string, 0)
-	stack := make([]*html.Node, 0)
 
-	stack = append([]*html.Node{node}, stack...)
-
-	for len(stack) != 0 {
-		currentNode := stack[0]
-
-		if len(stack) > 1 {
-			stack = stack[1:]
-		} else {
-			stack = stack[:0]
+	if node.Type == html.TextNode {
+		text := strings.TrimSpace(node.Data)
+		if text != "" {
+			textParts = append(textParts, text)
 		}
+	}
 
-		if currentNode.Type == html.TextNode {
-			text := strings.TrimSpace(currentNode.Data)
-			if text != "" {
-				textParts = append(textParts, text)
-			}
-		}
+	if node.FirstChild != nil {
+		textParts = append(textParts, extractTextParts(node.FirstChild)...)
+	}
 
-		if currentNode.NextSibling != nil && currentNode.Data != "a" {
-			stack = append([]*html.Node{currentNode.NextSibling}, stack...)
-		}
-
-		if currentNode.FirstChild != nil {
-			stack = append([]*html.Node{currentNode.FirstChild}, stack...)
-		}
+	if node.NextSibling != nil && node.Data != "a" {
+		textParts = append(textParts, extractTextParts(node.NextSibling)...)
 	}
 
 	return textParts
